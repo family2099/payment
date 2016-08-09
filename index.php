2cc1d9cc9e1042b6f5721d062cbe367cc0cfd0a1
Ricky <a686868520@gmail.com>
2016-08-09 08:13:05 +0000

    修改index.php的空白

--- index.php
+++ index.php
@@ -17,9 +17,7 @@
 
 <!DOCTYPE>
 <html>
-
 <head>
-    
 	<meta charset="utf-8">
 	<!---PS:其他js檔都要放JQUERY後面要不其他js檔先執行會錯誤--->
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -28,40 +26,30 @@
 	<script src="js/jquery.js"></script>
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<script src="views/js/bootstrap.min.js"></script>
-
 </head>
 
 <body>
-    
     <div class="row">
-        
         <div class="col-xm-12 col-sm-10 col-sm-offset-1 col-md-9 col-md-offset-1 col-lg-8 col-lg-offset-2">
-
                 <div class="panel panel-info">
-                      
                         <div class="panel-heading">
-                            
                             <div class="page-header">
                                 
                                 <center><h1>銀行簡易系統</h1><center>
                                 
                             </div>
-                              
                         </div>
 
                         <div>
                             <form role="form" method="post">
-                                
                                 <div class="form-group">
                                     
                                     <label>使用者:123</label>
                                 
                                 </div>
-                                
                             </form>
                             
                             <form role="form" method="post">    
-                                
                                 <div class="form-group">
                                     
                                     <label>存款金額</label>
@@ -70,7 +58,6 @@
                                 </div>
                                 
                                 <input type="hidden" name="accountSave" value="1">
-                                
                                 <button type="submit" class="btn btn-default" name="save" >存款</button>
                                 
                             </form>
